6686ce5f5f129bbb6d0d048a3bd63a75a55fedb9
Alex <alexprengere@gmail.com>
2016-06-13 05:11:10 -0400

    Fix PEP8

--- inject.py
+++ inject.py
@@ -1,57 +1,56 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 This module injects delimiters into a positional
 data feed.
-'''
+"""
 
 import argparse
 
-
-DEF_SHIFT     = 0
+DEF_SHIFT = 0
 DEF_DELIMITER = '^'
 
+
 def handle_args():
-    '''Command line parsing.
-    '''
+    """Command line parsing.
+    """
     parser = argparse.ArgumentParser(description='Inject delimiter in positional data feed.')
 
     parser.add_argument('flow',
-        help = '''Path to the file containing the data. If set to
-                       "-", the script will read the standard input
-                        instead.''',
-        type = argparse.FileType('r'),
-        default = '-'
-    )
+                        help="""Path to the file containing the data. If set to
+                        "-", the script will read the standard input
+                        instead.""",
+                        type=argparse.FileType('r'),
+                        default='-')
 
     parser.add_argument('-r', '--reverse',
-                        help = '''Reverse injection, will remove delimiter and
+                        help="""Reverse injection, will remove delimiter and
                         use positional fields instead. Using this tool first in normal
                         mode, then in reverse modei, with the exact same options, should be
-                        an identity for the file.''',
-                        action = 'store_true')
+                        an identity for the file.""",
+                        action='store_true')
 
     parser.add_argument('-n', '--no-spaces',
-                        help = '''Strip all field spaces when injecting delimiters.''',
-                        action = 'store_true')
+                        help="""Strip all field spaces when injecting delimiters.""",
+                        action='store_true')
 
     parser.add_argument('-s', '--shift',
-                        help = '''Specify start point for counting indices.
-                        Default is %s.''' % DEF_SHIFT,
-                        type = int,
-                        default = DEF_SHIFT)
+                        help="""Specify start point for counting indices.
+                        Default is %s.""" % DEF_SHIFT,
+                        type=int,
+                        default=DEF_SHIFT)
 
     parser.add_argument('-d', '--delimiter',
-                        help = '''Specify delimiter. Default is %s.''' % DEF_DELIMITER,
-                        default = DEF_DELIMITER)
+                        help="""Specify delimiter. Default is %s.""" % DEF_DELIMITER,
+                        default=DEF_DELIMITER)
 
     parser.add_argument('-i', '--indices',
-                        help = '''Indices for injection. Accepts n arguments separated by
-                        spaces, like -i 2 3 7.''',
-                        type = int,
-                        nargs = '+',
-                        default = [])
+                        help="""Indices for injection. Accepts n arguments separated by
+                        spaces, like -i 2 3 7.""",
+                        type=int,
+                        nargs='+',
+                        default=[])
 
     parser.epilog = 'Example: head example.pos | %s - -i 7 15 23' % parser.prog
 
@@ -59,9 +58,6 @@ def handle_args():
 
 
 def inject(flow, delimiter, shift, indices, strip):
-    '''
-    Inject
-    '''
     for row in flow:
         row = list(row.rstrip())
 
@@ -77,11 +73,7 @@ def inject(flow, delimiter, shift, indices, strip):
 
 
 def position(flow, delimiter, shift, indices):
-    '''
-    Position
-    '''
     for row in flow:
-
         new_row = []
         ind = [shift] + indices
 
@@ -99,24 +91,13 @@ def position(flow, delimiter, shift, indices):
         print ''.join(new_row),
 
 
-
 def main():
-    '''Main.
-    '''
     args = handle_args()
-
-    flow      = args['flow']
-    delimiter = args['delimiter']
-    shift     = args['shift']
-    indices   = args['indices']
-    strip     = args['no_spaces']
-
     if args['reverse']:
-        position(flow, delimiter, shift, indices)
+        position(args['flow'], args['delimiter'], args['shift'], args['indices'])
     else:
-        inject(flow, delimiter, shift, indices, strip)
+        inject(args['flow'], args['delimiter'], args['shift'], args['indices'], args['no_spaces'])
 
 
 if __name__ == '__main__':
-
     main()
--- setup.py
+++ setup.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-'''
-Main installation file for Injector.
-'''
-
 from setuptools import setup
 
 setup(
-    name = 'Injector',
-    version = '0.1',
-    author = 'Alex Prengere',
-    author_email = 'alex.prengere@gmail.com',
-    description = 'Inject delimiter into positional data feed.',
+    name='Injector',
+    version='0.1',
+    author='Alex Prengere',
+    author_email='alex.prengere@gmail.com',
+    description='Inject delimiter into positional data feed.',
     # Manage standalone scripts
-    entry_points = {
+    entry_points={
         'console_scripts' : [
             'inject = inject:main'
         ]
     },
-    py_modules = [
+    py_modules=[
         'inject'
     ]
 )
-
